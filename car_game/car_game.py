14bc6d16edeb03c2d641211dfdf8649a5efd35cb
aliex456 <estebanelias27dev@gmail.com>
2022-08-21 14:30:30 -0500

    sensor installed

--- car_game/car_game.py
+++ car_game/car_game.py
@@ -4,6 +4,7 @@ from car_game.road_generator import RoadGenerator
 from car_game.road_enum import RoadEnum
 from pygame import Rect
 from pygame.sprite import Sprite
+from pygame import Surface
 import math
 
 # COLORS
@@ -11,6 +12,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (139, 139, 139)
 GREEN = (24, 107, 24)
+RED = (240, 34, 34)
+SENSOR_LINE_WIDTH = 20
 CAR_SPRITE_LOCATION = 'car_game/images/car.png'
 
 
@@ -18,7 +21,7 @@ class CarGame:
 
     def __init__(self, screen_size: Tuple[int, int], car_speed: int,
                  number_of_cars: int, frame_rate: int,
-                 road: RoadEnum):
+                 road: RoadEnum, sensor_threshold: int):
         pygame.init()
         self.car_speed = car_speed
         self.screen = pygame.display.set_mode(screen_size)
@@ -28,6 +31,7 @@ class CarGame:
         self.road_generator = RoadGenerator(screen_size)
         self.clock = pygame.time.Clock()
         self.frame_rate = frame_rate
+        self.sensor_threshold = sensor_threshold
 
     def start_game(self):
 
@@ -79,13 +83,16 @@ class CarGame:
             player_sprite.rect = player_rect
             player_sprite.image = player_image_1_rot
 
-            print(pygame.sprite.collide_mask(map_sprite, player_sprite))
+            sensor_surface, surface_rect = self.__get_sensor_collision_distance((car_current_position_x, car_current_position_y), current_angle, map_sprite)
+
+            # print(pygame.sprite.collide_mask(map_sprite, player_sprite))
 
             # PAINT DISPLAY AND OBJECTS AND SET FRAMERATE
 
             self.screen.fill(GRAY)
             self.screen.blit(map_surface, map_sprite.rect)
             self.screen.blit(player_image_1_rot, player_rect)
+            self.screen.blit(sensor_surface, surface_rect)
             pygame.display.flip()
             self.clock.tick(self.frame_rate)
 
@@ -100,3 +107,20 @@ class CarGame:
         rotated_image = pygame.transform.rotate(image, angle)
         new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
         return rotated_image, new_rect
+
+    def __rot_from_side(self, image, angle, x, y):
+        rotated_image = pygame.transform.rotate(image, angle)
+        new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
+        return rotated_image, new_rect
+
+    def __get_sensor_collision_distance(self, car_position: [int, int], car_sensor_angle: int, map_sprite: Sprite) -> Tuple[Surface, Rect]:
+        sensor_surface = Surface((self.sensor_threshold, self.sensor_threshold*0.05))
+        sensor_rect = Rect(0, self.sensor_threshold/2, self.sensor_threshold, self.sensor_threshold/2)
+        pygame.draw.rect(sensor_surface, RED, sensor_rect, SENSOR_LINE_WIDTH)
+        surface_rect = Rect(car_position[0], car_position[1], self.sensor_threshold, self.sensor_threshold)
+        sensor_surface.fill(RED)
+        sensor_sprite = Sprite()
+        sensor_sprite.rect = surface_rect
+        sensor_sprite.image = sensor_surface
+        print(f'sensor collision {pygame.sprite.collide_mask(map_sprite, sensor_sprite)}')
+        return sensor_surface, surface_rect
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from car_game.road_enum import RoadEnum
 def main():
     car_game = CarGame(screen_size=(1600, 1200), car_speed=2,
                        number_of_cars=1, frame_rate=60,
-                       road=RoadEnum.ONE)
+                       road=RoadEnum.ONE, sensor_threshold=200)
 
     car_game.start_game()
 
