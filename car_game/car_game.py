e4e0a789ffab7c32b366f24d7643d9fd1e95645b
aliex456 <estebanelias27dev@gmail.com>
2022-08-21 19:55:08 -0500

    get collision distance

--- car_game/car_game.py
+++ car_game/car_game.py
@@ -15,6 +15,7 @@ GREEN = (24, 107, 24)
 RED = (240, 34, 34)
 SENSOR_LINE_WIDTH = 5
 CAR_SPRITE_LOCATION = 'car_game/images/car.png'
+MIDDLE_SENSOR_ANGLE = 60
 
 
 class CarGame:
@@ -83,8 +84,21 @@ class CarGame:
             player_sprite.rect = player_rect
             player_sprite.image = player_image_1_rot
 
-            sensor_surface, surface_rect = self.__get_sensor_collision_vector(
+            # COLLISION VECTORS
+
+            sensor_surface_vector_0, surface_rect_vector_0, collision_distance_vector_0 = \
+                self.__get_sensor_collision_vector(
                 (car_current_position_x, car_current_position_y), current_angle, map_sprite)
+
+            print(f'sensor collision {collision_distance_vector_0}')
+
+            sensor_surface_vector_middle, surface_rect_vector_middle, collision_distance_vector_middle = \
+                self.__get_sensor_collision_vector(
+                (car_current_position_x, car_current_position_y), current_angle+MIDDLE_SENSOR_ANGLE, map_sprite)
+
+            sensor_surface_vector_minus_middle, surface_rect_vector_minus_middle, collision_distance_vector_minus_middle = \
+                self.__get_sensor_collision_vector(
+                (car_current_position_x, car_current_position_y), current_angle-MIDDLE_SENSOR_ANGLE, map_sprite)
 
             # print(pygame.sprite.collide_mask(map_sprite, player_sprite))
 
@@ -93,7 +107,9 @@ class CarGame:
             self.screen.fill(GRAY)
             self.screen.blit(map_surface, map_sprite.rect)
             self.screen.blit(player_image_1_rot, player_rect)
-            self.screen.blit(sensor_surface, surface_rect)
+            self.screen.blit(sensor_surface_vector_0, surface_rect_vector_0)
+            self.screen.blit(sensor_surface_vector_middle, surface_rect_vector_middle)
+            self.screen.blit(sensor_surface_vector_minus_middle, surface_rect_vector_minus_middle)
             pygame.display.flip()
             self.clock.tick(self.frame_rate)
 
@@ -114,7 +130,8 @@ class CarGame:
         new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
         return rotated_image, new_rect
 
-    def __get_sensor_collision_vector(self, car_position: [int, int], car_sensor_angle: int, map_sprite: Sprite) -> Tuple[Surface, Rect]:
+    def __get_sensor_collision_vector(self, car_position: [int, int], car_sensor_angle: int, map_sprite: Sprite) \
+            -> Tuple[Surface, Rect, int]:
         sensor_surface = Surface((self.sensor_threshold, self.sensor_threshold), pygame.SRCALPHA)
         # sensor_surface.fill(BLACK)
         pygame.draw.line(sensor_surface, RED,
@@ -130,5 +147,20 @@ class CarGame:
         sensor_sprite = Sprite()
         sensor_sprite.rect = rotated_rect
         sensor_sprite.image = rotated_image
-        print(f'sensor collision {pygame.sprite.collide_mask(map_sprite, sensor_sprite)}')
-        return rotated_image, rotated_rect
+
+        collision_point = pygame.sprite.collide_mask(map_sprite, sensor_sprite)
+
+        distance_from_collision = -1
+
+        if collision_point:
+            distance_from_collision = CarGame.get_euclidean_distance(
+               collision_point,
+               (car_position[0], car_position[1])
+            )
+        return rotated_image, rotated_rect, distance_from_collision
+
+    @staticmethod
+    def get_euclidean_distance(point_1: Tuple[int, int], point_2: Tuple[int, int]) -> int:
+        square_x = math.pow((point_1[0] - point_2[0]), 2)
+        square_y = math.pow((point_1[1] - point_2[1]), 2)
+        return round(math.sqrt(square_x+square_y))
