937ec468a6260bf1b9036e9013acf2948aded893
Esteban Romero <estebanelias27dev@gmail.com>
2022-09-21 22:11:20 -0500

    adding new road

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from car_game.road_enum import RoadEnum
 
 def main():
     car_game = CarGame(screen_size=(1280, 720), car_speed=5, frame_rate=60,
-                       road=RoadEnum.THREE, sensor_threshold=200, generate_train_data=True, nn_model=None,
+                       road=RoadEnum.FIVE, sensor_threshold=200, generate_train_data=True, nn_model=None,
                        probability_to_decide=0.95)
 
     car_game.start_neat()
